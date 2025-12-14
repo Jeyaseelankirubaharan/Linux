f554fd64650bf584e14b0b4791129f186590b1fe
jeyaseelankirubaharan <jeyaseelan_3000@gmail.com>
2025-12-14 13:44:05 +0530

    pthread_thread_creation_fix

--- User_Space/my_service.cpp
+++ User_Space/my_service.cpp
@@ -10,7 +10,7 @@ void* my_thread(void*);
 
 int main(int argc , char * argv[]) {
 
-    pthread_t *threads[5]; int thread_status;
+    pthread_t threads[5]; int thread_status;
      pthread_attr_t attr;
 
     cout << "Hello World!" << argc << endl;
@@ -28,19 +28,32 @@ int main(int argc , char * argv[]) {
     {
 //    	sleep(1);
 //
-	thread_status = pthread_create(threads[i],&attr,my_thread,NULL);
+	thread_status = pthread_create(&threads[i],NULL,my_thread,NULL);
 	if(thread_status == 0)
 		printf("successfully created thread \n");
- 	my_mem = (int*)malloc(10);
-	free(my_mem);
+ 	/*my_mem = (int*)malloc(10);
+	free(my_mem);*/
 
     }
+
+    i = 0;
+
+    while(i++ < 5)
+    {
+    	if (pthread_join(threads[i], NULL)) {
+        fprintf(stderr, "Error joining thread\n");
+        return -1;
+    }
+    }
+
     return 0;
 }
 
 
 void* my_thread(void*)
 {
-
+	int *p; int a=10;
+	p = &a;
 	printf("My thread function\n");
+	return (void*) p; 
 }
